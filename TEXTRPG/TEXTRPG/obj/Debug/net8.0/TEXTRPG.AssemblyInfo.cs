1106d5049448e6e71d72c00da1bf8a45e8da48ae
nwoorang <89909664+nwoorang@users.noreply.github.com>
2025-04-21 11:16:25 +0900

    .
    .

--- TEXTRPG/TEXTRPG/obj/Debug/net8.0/TEXTRPG.AssemblyInfo.cs
+++ TEXTRPG/TEXTRPG/obj/Debug/net8.0/TEXTRPG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TEXTRPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d30406d1466c3baba6d2f6025f0223f8a249572")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b07e9e3796a2271ec9a4e4cfaf8b290ac13913b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TEXTRPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TEXTRPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
